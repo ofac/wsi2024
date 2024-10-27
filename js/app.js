6916ab4d2a5220a329df687d545bf988e3c45447
ofac <ofaczero@gmail.com>
2024-10-27 18:50:47 -0500

    Init files WSI Skill 17

---
+++ js/app.js
@@ -0,0 +1,40 @@
+
+// Scroll - - -
+window.addEventListener("scroll", () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
+// Scroll Gsap - - -
+gsap.registerPlugin(ScrollTrigger);
+window.addEventListener("load", () => {
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".wrapper",
+        start: "top top",
+        end: "+=150%",
+        pin: true,
+        scrub: true,
+        markers: false
+      }
+    })
+    .to("img.init", {
+      scale: 2,
+      z: 350,
+      transformOrigin: "center center",
+      ease: "power1.inOut"
+    })
+    .to(
+      ".section.hero",
+      {
+        scale: 1.1,
+        transformOrigin: "center center",
+        ease: "power1.inOut"
+      },
+      "<"
+    );
+});
